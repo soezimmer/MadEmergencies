70297b6c1e8c9d7f83990d6d7d7707488f16da33
soezimmer <108862945+soezimmer@users.noreply.github.com>
2023-11-24 12:59:31 +0100

    Now

--- CODE/main.py
+++ CODE/main.py
@@ -6,8 +6,8 @@ def main():
     num_citizens = 10  # Define the number of citizens for the simulation
     my_city = City(num_citizens)  # Create an instance of the City class
     my_city.start_services() 
-    #simulation = Simulation(my_city)  # Create an instance of the Simulation class with the city instance
-    #simulation.run()  # Run the simulation
+    simulation = Simulation(my_city)  # Create an instance of the Simulation class with the city instance
+    simulation.run()  # Run the simulation
 
 if __name__ == '__main__':
     normal_format = "\033[97m%(asctime)s:%(msecs)05d:  %(message)s\033[0m"
